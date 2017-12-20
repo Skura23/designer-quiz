4f7f087d9c463e3c366f29393cad1166cf14d80f
skura <776064932@qq.com>
2017-12-20 18:03:18 +0800

    modi

--- src/components/itemcontainer.vue
+++ src/components/itemcontainer.vue
@@ -1,7 +1,7 @@
 <template>
   	<section>
     	<header :class="{'top_tips':fatherComponent == 'item'}">
-    		<span class="num_tip" v-if="fatherComponent == 'item'">{{itemNum}}</span>
+    		<span class="num_tip" :class="{'item10':itemNum==10}" v-if="fatherComponent == 'item'">{{itemNum}}</span>
     	</header>
     	<div v-if="fatherComponent == 'home'" class="ov">
     		<!-- <div class="home_logo item_container_style"></div> -->
@@ -12,14 +12,20 @@
         <!-- <div class="" to="item"></div> -->
     	</div>
     	<div v-if="fatherComponent == 'item'" >
-        <div class="item-wra ov" :class="">
-          <p class="tc">safaf</p>
+        <div class="item-wra ov" :class="'ques'+itemNum">
+          <p class="tc">{{itemDetail[itemNum-1].topic_name}}</p>
           <div class="answers">
             <div class="ans-i">
+              <div class="tc mb" v-if="itemNum == 8">
+              <img width="60%" :src="'/static/'+itemDetail[itemNum-1].quesImg" alt="">
+              </div>
+              <p class="opt" v-for="(item, index) in itemDetail[itemNum-1].topic_answer" @click="choosed(index, item.topic_answer_id)">
+                <img v-if="item.answer_img" :src="'/static/'+item.answer_img" width="100%" alt="">
+                <span v-if="item.answer_name"><i>{{chooseType(index)}}.</i>{{item.answer_name}}</span>
+              </p>
+              <!-- <p>234</p>
               <p>234</p>
-              <p>234</p>
-              <p>234</p>
-              <p>234</p>
+              <p>234</p> -->
             </div>
           </div>
         </div>
@@ -60,12 +66,16 @@ export default {
     ...mapActions(["addNum", "initializeData"]),
     //点击下一题
     nextItem() {
-      if (this.choosedNum !== null) {
-        this.choosedNum = null;
-        //保存答案, 题目索引加一，跳到下一题
-        this.addNum(this.choosedId);
+      if (this.itemNum < this.itemDetail.length) {
+        if (this.choosedNum !== null) {
+          this.choosedNum = null;
+          //保存答案, 题目索引加一，跳到下一题
+          this.addNum(this.choosedId);
+        } else {
+          alert("您还没有选择答案哦");
+        }
       } else {
-        alert("您还没有选择答案哦");
+        this.submitAnswer();
       }
     },
     //索引0-3对应答案A-B
@@ -145,7 +155,12 @@ body {
     // color: #a57c50;
     text-align: center;
   }
+  .item10{
+    font-size: 1.4rem;
+    text-indent: -9px;
+  }
 }
+
 .item_container_style {
   height: 11.625rem;
   width: 13.15rem;
@@ -163,26 +178,26 @@ body {
 //   background-image: url(../images/2-1.png);
 //   background-size: 100% 100%;
 // }
-.button_style {
-  display: block;
-  height: 4.7rem;
-  width: 7.35rem;
-  background-size: 100% 100%;
-  position: absolute;
-  top: 16.9rem;
-  left: 58.8%;
-  margin-left: -2.4rem;
-  background-repeat: no-repeat;
-}
-.start {
-  background-image: url(../images/DIR/8.png);
-}
-.next_item {
-  background-image: url(../images/2-2.png);
-}
-.submit_item {
-  background-image: url(../images/3-1.png);
-}
+// .button_style {
+//   display: block;
+//   height: 4.7rem;
+//   width: 7.35rem;
+//   background-size: 100% 100%;
+//   position: absolute;
+//   top: 16.9rem;
+//   left: 58.8%;
+//   margin-left: -2.4rem;
+//   background-repeat: no-repeat;
+// }
+// .start {
+//   background-image: url(../images/DIR/8.png);
+// }
+// .next_item {
+//   background-image: url(../images/2-2.png);
+// }
+// .submit_item {
+//   background-image: url(../images/3-1.png);
+// }
 .item_list_container {
   position: absolute;
   height: 7rem;
@@ -237,8 +252,8 @@ body {
   border: 2px solid #0e050a;
   border-radius: 0.5rem;
   box-shadow: 0.17rem 0.17rem #deaf1f;
-  >p {
-    padding: 0.7rem 0;
+  > p {
+    padding: 0.7rem 0.6rem;
     background: #0e050a;
     color: #ffe100;
     &:before {
@@ -253,30 +268,154 @@ body {
       background-size: contain;
     }
   }
+  // 第一题的answers样式
+  // 无背景, 有虚线, 为absolute
+  &.ques1,
+  &.ques3,
+  &.ques6,
+  &.ques9 {
+    .answers {
+      height: 12rem;
+      .ans-i {
+        p {
+          width: 4.5rem;
+          height: 4.5rem;
+          position: absolute;
+          background: none;
+          &:first-child {
+            left: 0;
+            top: 0;
+          }
+          &:nth-child(2) {
+            right: 0;
+            top: 0;
+          }
+          &:nth-child(3) {
+            left: 0;
+            bottom: 0;
+          }
+          &:last-child {
+            right: 0;
+            bottom: 0;
+          }
+        }
+        &:before {
+          content: " ";
+          display: block;
+          position: absolute;
+          width: 100%;
+          border: 1px dashed #393939;
+          top: 50%;
+        }
+        &:after {
+          content: " ";
+          display: block;
+          position: absolute;
+          height: 100%;
+          border: 1px dashed #393939;
+          left: 50%;
+          top: 0;
+        }
+      }
+    }
+  }
+
+  &.ques3,
+  &.ques9 {
+    .ans-i {
+      p.opt {
+        margin-bottom: 0.5rem;
+        width: 3.9rem;
+        height: 3.9rem;
+        span {
+          position: absolute;
+          display: inline-block;
+          width: 100%;
+          text-align: center;
+          left: 0;
+          bottom: -0.9rem;
+          font-size: 0.6rem;
+        }
+      }
+    }
+  }
+  &.ques4 {
+    > p {
+      padding: 0.7rem 1.2rem;
+    }
+  }
+  &.ques5 {
+    > p {
+      font-size: 0.8rem;
+    }
+  }
+  &.ques6 {
+    .answers {
+      height: 10.3rem;
+      p.opt {
+        height: auto;
+      }
+    }
+  }
+  &.ques7 {
+    p.opt {
+      font-size: 0.7rem;
+      &:nth-child(3){
+        padding-left: 1.3rem!important;
+        padding-right: 1.3rem!important;
+      }
+    }
+  }
+  &.ques8 {
+    .ans-i{
+      // text-align: center;
+      overflow: hidden;
+    }
+    p.opt {
+      float: left;
+      font-size: 0.6rem;
+      width: 4.7rem;
+      &:nth-child(3),&:nth-child(5){
+        margin-left: 0.8rem;
+      }
+    }
+  }  
+
+  // 第二题及其他纯文字题的answers样式
+  // 有背景, 有虚线, 非absolute
+  &.ques2,
+  &.ques4,
+  &.ques5,
+  &.ques7,
+  &.ques8,
+  &.ques10 {
+    .answers {
+      background: #fff;
+      padding: 1rem;
+      .ans-i {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        > p {
+          background: url(/static/10.png) no-repeat;
+          background-size: 100% 100%;
+          padding: 8px;
+          text-align: center;
+          margin-bottom: 0.6rem;
+          &:last-child {
+            margin-bottom: 0rem;
+          }
+        }
+      }
+    }
+  }
   .answers {
     background: #fff;
     padding: 1rem;
-    .ans-i{
+    .ans-i {
       width: 100%;
       height: 100%;
       position: relative;
-      &:before{
-        content: " ";
-        display: block;
-        position: absolute;
-        width: 100%;
-        border: 2px dashed #393939;
-        top: 50%;
-      }
-      &:after{
-        content: " ";
-        display: block;
-        position: absolute;
-        height: 100%;
-        border: 2px dashed #393939;
-        left: 50%;
-        top: 0;
-      }
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -25,25 +25,25 @@ const state = {
 		"topic_answer": [{
 			"topic_answer_id": 1,
 			"topic_id": 20,
-			"answer_name": "答案aaaa",
-			"answer_img": "1_2.png",
+			"answer_name": "",
+			"answer_img": "1_1.png",
 			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 2,
 			"topic_id": 20,
-			"answer_name": "正确答案",
-			"answer_img": "1_1.png",
+			"answer_name": "",
+			"answer_img": "1_2.png",
 			"is_standard_answer": 1
 		}, {
 			"topic_answer_id": 3,
 			"topic_id": 20,
-			"answer_name": "答案cccc",
+			"answer_name": "",
 			"answer_img": "1_3.png",
 			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 4,
 			"topic_id": 20,
-			"answer_name": "答案dddd",
+			"answer_name": "",
 			"answer_img": "1_4.png",
 			"is_standard_answer": 0
 		}]
@@ -51,7 +51,7 @@ const state = {
 		"topic_id": 21,
 		"active_topic_id": 4,
 		"type": "MORE",
-		"topic_name": "2",
+		"topic_name": "下面哪个网站可以让设计作品实现最大的经济价值？",
 		"active_id": 1,
 		"active_title": "欢乐星期五标题",
 		"active_topic_phase": "第一周",
@@ -60,29 +60,29 @@ const state = {
 		"topic_answer": [{
 			"topic_answer_id": 5,
 			"topic_id": 21,
-			"answer_name": "答案A",
-			"is_standard_answer": 1
+			"answer_name": "淘宝",
+			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 6,
 			"topic_id": 21,
-			"answer_name": "答案B",
+			"answer_name": "站酷",
 			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 7,
 			"topic_id": 21,
-			"answer_name": "正确答案",
+			"answer_name": "猪八戒网",
 			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 8,
 			"topic_id": 21,
-			"answer_name": "答案D",
-			"is_standard_answer": 0
+			"answer_name": "自个网",
+			"is_standard_answer": 1
 		}]
 	}, {
 		"topic_id": 21,
 		"active_topic_id": 4,
 		"type": "MORE",
-		"topic_name": "3",
+		"topic_name": "下面哪个颜色不是Pantone发布的2018年流行色",
 		"active_id": 1,
 		"active_title": "欢乐星期五标题",
 		"active_topic_phase": "第一周",
@@ -91,29 +91,33 @@ const state = {
 		"topic_answer": [{
 			"topic_answer_id": 9,
 			"topic_id": 21,
-			"answer_name": "测试A",
-			"is_standard_answer": 1
+			"answer_name": "青柠调酒黄",
+			"answer_img": "4_1.png",
+			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 10,
 			"topic_id": 21,
-			"answer_name": "BBBBBB",
+			"answer_name": "紫外光",
+			"answer_img": "4_2.png",
 			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 11,
 			"topic_id": 21,
-			"answer_name": "CCCCCC",
-			"is_standard_answer": 0
+			"answer_name": "树脂绿",
+			"answer_img": "4_3.png",
+			"is_standard_answer": 1
 		}, {
 			"topic_answer_id": 12,
 			"topic_id": 21,
-			"answer_name": "正确答案",
+			"answer_name": "暖沙色",
+			"answer_img": "4_4.png",
 			"is_standard_answer": 0
 		}]
 	}, {
 		"topic_id": 21,
 		"active_topic_id": 4,
 		"type": "MORE",
-		"topic_name": "4",
+		"topic_name": "下面哪个不是平面设计的三要素",
 		"active_id": 1,
 		"active_title": "欢乐星期五标题",
 		"active_topic_phase": "第一周",
@@ -122,29 +126,29 @@ const state = {
 		"topic_answer": [{
 			"topic_answer_id": 13,
 			"topic_id": 21,
-			"answer_name": "正确答案",
-			"is_standard_answer": 1
+			"answer_name": "图片",
+			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 14,
 			"topic_id": 21,
-			"answer_name": "A是错的",
+			"answer_name": "字体",
 			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 15,
 			"topic_id": 21,
-			"answer_name": "D是对的",
-			"is_standard_answer": 0
+			"answer_name": "配色",
+			"is_standard_answer": 1
 		}, {
 			"topic_answer_id": 16,
 			"topic_id": 21,
-			"answer_name": "C说的不对",
+			"answer_name": "排版",
 			"is_standard_answer": 0
 		}]
 	}, {
 		"topic_id": 21,
 		"active_topic_id": 4,
 		"type": "MORE",
-		"topic_name": "5",
+		"topic_name": "传说设计师的A站是Artstation  B站是Behance、D站是dribbble那么 同理可得,Z站是什么？",
 		"active_id": 1,
 		"active_title": "欢乐星期五标题",
 		"active_topic_phase": "第一周",
@@ -153,26 +157,195 @@ const state = {
 		"topic_answer": [{
 			"topic_answer_id": 17,
 			"topic_id": 21,
-			"answer_name": "错误答案",
-			"is_standard_answer": 1
+			"answer_name": "ZOL",
+			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 18,
 			"topic_id": 21,
-			"answer_name": "正确答案",
-			"is_standard_answer": 0
+			"answer_name": "zigeer",
+			"is_standard_answer": 1
 		}, {
 			"topic_answer_id": 19,
 			"topic_id": 21,
-			"answer_name": "错误答案",
+			"answer_name": "Zaker",
 			"is_standard_answer": 0
 		}, {
 			"topic_answer_id": 20,
 			"topic_id": 21,
-			"answer_name": "错误答案",
+			"answer_name": "Zhihu",
 			"is_standard_answer": 0
 		}]
-	}],
+	},
+	{
+		"topic_id": 21,
+		"active_topic_id": 4,
+		"type": "MORE",
+		"topic_name": "下面哪个Adode CC 2018的启动画面是错误的",
+		"active_id": 1,
+		"active_title": "欢乐星期五标题",
+		"active_topic_phase": "第一周",
+		"active_start_time": "1479139200",
+		"active_end_time": "1482163200",
+		"topic_answer": [{
+			"topic_answer_id": 21,
+			"topic_id": 21,
+			"answer_name": "",
+			"answer_img": "2_1.png",
+			"is_standard_answer": 0
+		}, {
+			"topic_answer_id": 22,
+			"topic_id": 21,
+			"answer_name": "",
+			"answer_img": "2_2.png",
+			"is_standard_answer": 1
+		}, {
+			"topic_answer_id": 23,
+			"topic_id": 21,
+			"answer_name": "",
+			"answer_img": "2_3.png",
+			"is_standard_answer": 0
+		}, {
+			"topic_answer_id": 24,
+			"topic_id": 21,
+			"answer_name": "",
+			"answer_img": "2_4.png",
+			"is_standard_answer": 0
+		}]
+	}, {
+		"topic_id": 21,
+		"active_topic_id": 4,
+		"type": "MORE",
+		"topic_name": "下面哪本书对应的作者是错误的？",
+		"active_id": 1,
+		"active_title": "欢乐星期五标题",
+		"active_topic_phase": "第一周",
+		"active_start_time": "1479139200",
+		"active_end_time": "1482163200",
+		"topic_answer": [{
+			"topic_answer_id": 25,
+			"topic_id": 21,
+			"answer_name": "《为什么设计》—松田行正",
+			"is_standard_answer": 1
+		}, {
+			"topic_answer_id": 26,
+			"topic_id": 21,
+			"answer_name": "《设计中的设计》—原研哉",
+			"is_standard_answer": 0
+		}, {
+			"topic_answer_id": 27,
+			"topic_id": 21,
+			"answer_name": "《写给大家看的设计书》—Robin Williams",
+			"is_standard_answer": 0
+		}, {
+			"topic_answer_id": 28,
+			"topic_id": 21,
+			"answer_name": "《设计的觉醒》—田中一光",
+			"is_standard_answer": 0
+		}]
+	},{
+		"topic_id": 21,
+		"active_topic_id": 4,
+		"type": "MORE",
+		"topic_name": "下面这张图属于什么风格？",
+		"active_id": 1,
+		"active_title": "欢乐星期五标题",
+		"active_topic_phase": "第一周",
+		"active_start_time": "1479139200",
+		"active_end_time": "1482163200",
+		"quesImg":'9.png',
+		"topic_answer": [{
+			"topic_answer_id": 29,
+			"topic_id": 21,
+			"answer_name": "蒸汽波艺术",
+			"is_standard_answer": 0
+		}, {
+			"topic_answer_id": 30,
+			"topic_id": 21,
+			"answer_name": "孟菲斯艺术",
+			"is_standard_answer": 0
+		}, {
+			"topic_answer_id": 31,
+			"topic_id": 21,
+			"answer_name": "MBE插画风格",
+			"is_standard_answer": 0
+		}, {
+			"topic_answer_id": 32,
+			"topic_id": 21,
+			"answer_name": "Low poly风格",
+			"is_standard_answer": 1
+		}]
+	},
+	{
+		"topic_id": 21,
+		"active_topic_id": 4,
+		"type": "MORE",
+		"topic_name": "下面哪个设计师对应的作品是错误的？",
+		"active_id": 1,
+		"active_title": "欢乐星期五标题",
+		"active_topic_phase": "第一周",
+		"active_start_time": "1479139200",
+		"active_end_time": "1482163200",
+		"topic_answer": [{
+			"topic_answer_id": 33,
+			"topic_id": 21,
+			"answer_name": "村上隆",
+			"answer_img": "3_1.png",
+			"is_standard_answer": 0
+		}, {
+			"topic_answer_id": 34,
+			"topic_id": 21,
+			"answer_name": "草间弥生",
+			"answer_img": "3_2.png",
+			"is_standard_answer": 0
+		}, {
+			"topic_answer_id": 35,
+			"topic_id": 21,
+			"answer_name": "杉浦康平",
+			"answer_img": "3_3.png",
+			"is_standard_answer": 1
+		}, {
+			"topic_answer_id": 36,
+			"topic_id": 21,
+			"answer_name": "田中一光",
+			"answer_img": "3_4.png",
+			"is_standard_answer": 0
+		}]
+	},
+	{
+		"topic_id": 21,
+		"active_topic_id": 4,
+		"type": "MORE",
+		"topic_name": "你觉得下面有几点是AI设计师鲁班望尘莫及、无法取代的？",
+		"active_id": 1,
+		"active_title": "欢乐星期五标题",
+		"active_topic_phase": "第一周",
+		"active_start_time": "1479139200",
+		"active_end_time": "1482163200",
+		"topic_answer": [{
+			"topic_answer_id": 37,
+			"topic_id": 21,
+			"answer_name": "颜值",
+			"is_standard_answer": 1
+		}, {
+			"topic_answer_id": 38,
+			"topic_id": 21,
+			"answer_name": "发量",
+			"is_standard_answer": 1
+		}, {
+			"topic_answer_id": 39,
+			"topic_id": 21,
+			"answer_name": "对设计的热爱",
+			"is_standard_answer": 1
+		}, {
+			"topic_answer_id": 40,
+			"topic_id": 21,
+			"answer_name": "对甲方的恨",
+			"is_standard_answer": 1
+		}]
+	}
+],
 	answerid: [], //答案id
+	// quesImg
 }
 
 export default new Vuex.Store({
